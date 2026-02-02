6e0439a1189a3182f1f907ae26b9b04c28b448de
mateocasta33 <162051608+mateocasta33@users.noreply.github.com>
2026-02-02 14:35:10 -0500

    Configure dependency injection: added services and repositories

--- workplace.Api/Program.cs
+++ workplace.Api/Program.cs
@@ -2,13 +2,43 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using workplace.Application.Dtos;
+using workplace.Application.Interfaces;
+using workplace.Application.Services;
+using workplace.Domain.Interfaces;
+using workplace.Infrastructure.Repositories;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+        {
+            services.AddApplicationInsightsTelemetryWorkerService();
+            services.ConfigureFunctionsApplicationInsights();
 
-builder.ConfigureFunctionsWebApplication();
+            services.AddHttpContextAccessor();
 
-builder.Services
-    .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
-
-builder.Build().Run();
+            // AutoMapper
+            services.AddAutoMapper(typeof(MapProfile));
+            
+            // Repositories
+            services.AddScoped<IUserRepository, UserRepository>();
+            
+            // Service
+            services.AddScoped<IUserService, UserService>();
+            
+            // CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+            
+        })
+    .Build();
+    
+host.Run();
